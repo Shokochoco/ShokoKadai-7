26acaef926bce174490354b97fb311b57783fbf4
akio0911 <akio0911@gmail.com>
2021-12-09 12:29:29 +0900

    feedback

--- Kadai-7/ViewControllers/RedViewController.swift
+++ Kadai-7/ViewControllers/RedViewController.swift
@@ -16,13 +16,8 @@ class RedViewController: UIViewController {
         guard let num1 = Int(textField1.text!) else { return }
         guard let num2 = Int(textField2.text!) else { return }
 
-        totalLabel.text = calculate(num1: num1, num2: num2)
+        totalLabel.text = String(Calculator().addition(value1: num1, value2: num2))
         textField1.endEditing(true)
         textField2.endEditing(true)
-    }
-
-    func calculate(num1: Int, num2: Int) -> String {
-        let totalNum =  num1 + num2
-        return String(totalNum)
     }
 }
